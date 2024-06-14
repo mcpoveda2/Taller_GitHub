3c5af808948b6febf012469ef426d3b300e8ae47
Laurita2014 <lauol_86@hotmail.com>
2024-06-13 22:57:50 -0500

    Create Operaciones con listas.py

---
+++ Operaciones con listas.py
@@ -0,0 +1,17 @@
+primera función:
+def suma_lista(lista):
+  total = 0
+  for numero in lista:
+    total += numero
+  return total
+
+
+segunda funcion:
+def mayor_elemento(lista)
+  if not lista:
+    return None 
+  mayor = lista [0]
+  for numero in lista:
+      if numero > mayor:
+        mayor = numero
+  return mayor
